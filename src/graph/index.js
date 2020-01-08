e169a7ee16253d5dfc3efcf82a37ae3184dcbfd4
Hung Ho <hungho@Hungs-MacBook-Pro.local>
2020-01-09 01:41:08 +0200

    Update initial data

--- src/graph/index.js
+++ src/graph/index.js
@@ -14,15 +14,18 @@ if (savedData) {
 } else {
   tasks.forEach(task => graph.addNode(new Node({...task})));
   graph.addConnection('t0', 't1');
-  graph.addConnection('t0', 't2');
   graph.addConnection('t1', 't3');
   graph.addConnection('t1', 't5');
-  graph.addConnection('t2', 't4');
-  graph.addConnection('t2', 't6');
   graph.addConnection('t3', 't7');
-  graph.addConnection('t3', 't8');
   graph.addConnection('t7', 't9');
   graph.addConnection('t7', 't10');
+  graph.addConnection('t3', 't8');
+  graph.addConnection('t8', 't11');
+
+  graph.addConnection('t0', 't2');
+  graph.addConnection('t2', 't4');
+  graph.addConnection('t2', 't5');
+  graph.addConnection('t2', 't6');
 }
 
 export default graph;
